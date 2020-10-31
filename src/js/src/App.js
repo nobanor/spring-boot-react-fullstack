4a9fc84eae288fb02dd8813463bb66a8fed12354
Nino Obanor <nino.obanor@perficient.com>
2020-10-31 18:38:23 -0500

    Fetch data

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
 import './App.css';
+import {getAllStudents} from './client';
+import React, { Component } from 'react';
+import {
+  Table,
+  Avatar,
+  Spin,
+  antIcon
+} from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Container from './Container';
+const getIndicatorAntIcon = () => <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+class App extends Component{
+  
+  state = {
+    students: [],
+    isFetching: false
+  }
+
+  componentDidMount () {
+    this.fetchStudents();
+  }
+
+  fetchStudents = () => {
+    this.setState({
+      isFetching: true
+    });
+    getAllStudents()
+      .then(res => res.json()
+      .then(students => {
+        console.log(students);
+        this.setState({
+          students,
+          isFetching: false
+        });
+    }));
+  }
+
+  render(){
+    const { students, isFetching } = this.state;
+
+    if(isFetching){
+      return(
+        <Container>
+          <Spin indicator={getIndicatorAntIcon()}/>
+        </Container>
+      )
+    }
+    if(students && students.length){
+        const columns = [
+          {
+            title:'',
+            key:'avatar',
+            render: (text, student) =>{
+              <Avatar size='large'>
+                {`${student.firstName.charAt(0).toUpperCase()} ${student.lastName.charAt(0).toUpperCase()}`}
+              </Avatar>
+            }
+          },
+          {
+            title: 'Student Id',
+            dataIndex: 'studentId',
+            key: 'studentId'
+          },
+          {
+            title: 'First Name',
+            dataIndex: 'firstName',
+            key: 'firstName'
+          },
+          {
+            title: 'Last Name',
+            dataIndex: 'lastName',
+            key: 'lastName'
+          },
+          {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email'
+          },
+          {
+            title: 'Gender',
+            dataIndex: 'gender',
+            key: 'gender'
+          }
+        ];
+
+        return (
+          <Container>
+            <Table 
+            dataSource={students} 
+            columns={columns} 
+            pagination={false}
+            rowKey='studentId' 
+          />
+          </Container>
+        );
+    }
+
+    return (
+    <h1> No Students Found</h1>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/js/src/client.js
@@ -0,0 +1,18 @@
+import fetch from 'unfetch';
+
+const checkStatus = response => {
+    if (response.ok) {
+        return response;
+    } else {
+        let error = new Error(response.statusText);
+        error.response = response;
+        response.json().then(e => {
+            error.error = e;
+        });
+        return Promise.reject(error);
+    }
+}
+
+export const getAllStudents = () => 
+    fetch('/students').then(checkStatus).then(checkStatus);
+
